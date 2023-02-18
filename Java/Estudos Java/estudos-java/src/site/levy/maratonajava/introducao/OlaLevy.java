a26351ee613f352e5e8721322c063696e3037956
Levy Martins Nascimento <levymnascimento@hotmail.com>
2023-02-17 21:03:15 -0300

    Revert "Estudando Tipos primitivos e estrutura condicional"
    This reverts commit e104432e8c8afb606f47cfb4c88b6a39f4b1ddbe.

--- Java/Estudos Java/estudos-java/src/site/levy/maratonajava/introducao/OlaLevy.java
+++
@@ -1,7 +0,0 @@
-package site.levy.maratonajava.introducao;
-
-public class OlaLevy {
-    public static void main(String[] args) {
-        System.out.println("KA-ME-HA-ME-HAAAAAAAAAAAAAA");
-    }
-}
--- Java/Estudos Java/estudos-java/src/site/levy/maratonajava/introducao/TiposPrimitivos.java
+++
@@ -1,9 +0,0 @@
-package site.levy.maratonajava.introducao;
-
-public class TiposPrimitivos {
-    public static void main(String[] args) {
-        int age = 10;
-
-        System.out.println("Your age is " + age + " years");
-    }
-}
